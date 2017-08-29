cc6d5d4c49c852e8a04f2b2cc6b45bfcfa8593c9
freudFlintstone <raphael.mattos@contextaware.com.br>
2017-08-29 02:18:26 -0300

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # pjbank-python-sdk
 PJbank SDK para Python! :snake: :snake: :snake:
+
+![Construcao](https://openclipart.org/image/2400px/svg_to_png/231626/underconstruction.png)
---
+++ pjbanksdk/config.py
@@ -0,0 +1,7 @@
+__version__ = "0.1.dev"
+__packagename__ = "pjbanksdk"
+__pypi_packagename__ = "pjbank-sdk"
+__endpoint_map__ = {
+    "live": "https://api.pjbank.com.br",
+    "sandbox": "https://sandbox.pjbank.com.br",
+}
