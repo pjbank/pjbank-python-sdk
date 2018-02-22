6f8860615e4d70201d9152dd298e4576b68fd3e5
Raphael Mattos <raphael.mattos@contextaware.com.br>
2018-02-22 19:21:10 -0300

    no install travis

--- tests/test_recebimentos.py
+++ tests/test_recebimentos.py
@@ -2,18 +2,28 @@
 # testing in general, but rather to support the `find_packages` example in
 # setup.py that excludes installing the "tests" package
 import unittest
+import pickle
 from datetime import datetime as datetime       
 from pjbank import Boleto
 
-class BoletoTestCase(unittest.TestCase):
+class DadosTeste(object):
     def __init__(self):
-        super(BoletoTestCase, self).__init__()
-        self.dados = tests.dados
-        creds = dados['recebimentos']['boleto']['credenciamento']
+        super(DadosTeste, self).__init__()
+        with open('dados_testes.pkl', 'rb') as dump:
+            self._dados = pickle.load(dump)
+    
+    def getDados(self):
+        return self._dados
+        
+        
+class BoletoTestCase(unittest.TestCase):
+    def setUp(self):
+        self.dados = DadosTeste().getDados()
+        creds = self.dados['recebimentos']['boleto']['credenciamento']
         self.conta = pjbank.Boleto(creds['credencial'], creds['chave'])
     
     def test_dados(self):
-        self.assertGreaterEqual(len(self.dados) >)
+        self.assertGreaterEqual(len(self.dados) > 0)
         self.assert_(self.conta.credencial)
         self.assert_(self.conta.chave)
     
